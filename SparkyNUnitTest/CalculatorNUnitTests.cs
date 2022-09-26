034ddc2c3581a2579fcf9c44d1b8219970eb11c5
Leandro Barros Pontes <65022896+LeandroBPontes@users.noreply.github.com>
2022-09-26 20:40:08 -0300

    new units tests

--- SparkyNUnitTest/CalculatorNUnitTests.cs
+++ SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -20,4 +20,34 @@ public class CalculatorNUnitTests
         Assert.AreEqual(30, result);
         
     }
+
+    [Test]
+    public void IsOddChecker_InputEvenNumber_ReturnFalse()
+    {
+        Calculator calc = new();
+        bool isOdd = calc.IsOddNumber(10);
+        Assert.That(isOdd, Is.EqualTo(false));
+        Assert.IsFalse(isOdd);
+    }
+    
+    [Test]
+    [TestCase(11)]
+    [TestCase(13)]
+    [TestCase(15)]
+    public void IsOddChecker_InputEvenNumber_ReturnTrue(int a)
+    {
+        Calculator calc = new();
+        bool isOdd = calc.IsOddNumber(a);
+        Assert.That(isOdd, Is.EqualTo(true));
+        Assert.IsTrue(isOdd);
+    }
+    
+    [Test]
+    [TestCase(10, ExpectedResult = false)]
+    [TestCase(11, ExpectedResult = true)]
+    public bool IsOddChecker_InputEvenNumber_ReturnTrueIfOdd(int a)
+    {
+        Calculator calc = new();
+        return calc.IsOddNumber(a);
+    }
 }
